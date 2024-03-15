929d0260673491258b7be4c11005f4013f606a2e
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-15 17:49:35 -0400

    Added chef specialities, poultry and luncheon specials

--- client/src/components/Content.jsx
+++ client/src/components/Content.jsx
@@ -1,23 +1,24 @@
 import '../styles/Content.css'
-import { menuItems } from '../jsFiles/menuData';
+import { chefSpecialities } from '../jsFiles/chefSpecialities';
 import { glutenFree } from '../jsFiles/glutenFree';
 import { appetizers } from '../jsFiles/appetizers';
+import { poultry } from '../jsFiles/poultry';
 import { beefAndLamb } from '../jsFiles/beefAndLamb';
 import { FriedRiceNoodlesChowMein } from '../jsFiles/FriedRiceNoodlesChowMein';
 import { seafood } from '../jsFiles/seafood';
 import { pork } from '../jsFiles/pork';
 import { vegetables } from '../jsFiles/vegetables';
 import { thaiSpecials } from '../jsFiles/thaiSpecials';
+import { luncheonSpecial } from '../jsFiles/luncheonSpecial';
 
 function Content(){
     return(
         <div className="content">
-            <h2 className='menu_gluten'>Menu</h2>
+            <h3 className='checfSpecialities'>Chef Specialities</h3>
             <ul>
-                {menuItems.map((item) => (
+                {chefSpecialities.map((item) => (
                     <li key={item.name}>
-                        <h2>{item.name}</h2>
-                        <p>Ingredients: {item.ingredients}</p>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
@@ -41,56 +42,74 @@ function Content(){
                     </li>
                 ))}
             </ul>
-            <h2 className="beef_and_lamb">Beef and Lamb</h2>
+            <h3 className="poultry">Poultry</h3>
+            <ul>
+                {poultry.map((item) => (
+                    <li key = {item.name}>
+                        <h3>{item.name}</h3>
+                        <span>${item.price}</span>
+                    </li>
+                ))}
+            </ul>
+            <h3 className="beef_and_lamb">Beef and Lamb</h3>
             <ul>
                 {beefAndLamb.map((item) => (
                     <li key = {item.name}>
-                        <h2>{item.name}</h2>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
             </ul>
-            <h2 className="seafood">Seafood</h2>
+            <h3 className="seafood">Seafood</h3>
             <ul>
                 {seafood.map((item) => (
                     <li key = {item.name}>
-                        <h2>{item.name}</h2>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
             </ul>
-            <h2 className="pork">Pork</h2>
+            <h3 className="pork">Pork</h3>
             <ul>
                 {pork.map((item) => (
                     <li key = {item.name}>
-                        <h2>{item.name}</h2>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
             </ul>
-            <h2 className="vegetables">Vegetables</h2>
+            <h3 className="vegetables">Vegetables</h3>
             <ul>
                 {vegetables.map((item) => (
                     <li key = {item.name}>
-                        <h2>{item.name}</h2>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
             </ul>
-            <h2 className="fried_rice_noodles_chow_mein">Fried Rice, Noodles, Chow Mein</h2>
+            <h3 className="fried_rice_noodles_chow_mein">Fried Rice, Noodles, Chow Mein</h3>
             <ul>
                 {FriedRiceNoodlesChowMein.map((item) => (
                     <li key = {item.name}>
-                        <h2>{item.name}</h2>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
             </ul>
-            <h2 className="thai_specials">Thai Specials</h2>
+            <h3 className="thai_specials">Thai Specials</h3>
             <ul>
                 {thaiSpecials.map((item) => (
                     <li key = {item.name}>
-                        <h2>{item.name}</h2>
+                        <h3>{item.name}</h3>
+                        <span>${item.price}</span>
+                    </li>
+                ))}
+            </ul>
+            <h3 className="luncheon_special">Luncheon Special</h3>
+            <ul>
+                {luncheonSpecial.map((item) => (
+                    <li key = {item.name}>
+                        <h3>{item.name}</h3>
                         <span>${item.price}</span>
                     </li>
                 ))}
---
+++ client/src/jsFiles/chefSpecialities.js
@@ -0,0 +1,114 @@
+export const chefSpecialities = [
+    {
+        name: "House Special Chicken",
+        price: 17,
+        description: "Tender chicken chunks fried to a crisp & sautéed w rich garlic sauce.",
+        spice: null
+    },
+    {
+        name: "Vegetable Home Style",
+        price: 15,
+        description: "Broccoli, snow peas, zucchini, cabbage, fried tofu, mushrooms & carrots sautéed with gourmet hot garlic sauce.",
+        spice: null
+    },
+    {
+        name: "Champagne Chicken",
+        price: 17,
+        description: "Sliced white meat chicken, snow peas, water chestnuts & mushrooms sautéed with Chinese rice wine sauce.",
+        spice: null
+    },
+    {
+        name: "Shrimp & Chicken w. Basil",
+        price: 18,
+        description: "Large shrimp, white meat chicken & fresh garlic sautéed with basil sauce.",
+        spice: null
+    },
+    {
+        name: "Mongolian Trio",
+        price: 18,
+        description: "Beef, shrimp, chicken, scallions and onions sautéed w. light brown sauce.",
+        spice: null
+    },
+    {
+        name: "Coral Shrimp",
+        price: 18,
+        description: "Lightly battered shrimp stir-fried with red pepper, snow peas, celery & shredded ginger in Chef's special ginger sauce.",
+        spice: null
+    },
+    {
+        name: "Golden Sesame Chicken",
+        price: 17,
+        description: "Triple delight of shrimp, chicken and beef sautéed with broccoli, baby corn and mushrooms in a rich brown sauce.",
+        spice: null
+    },
+    {
+        name: "Triple Delight",
+        price: 18,
+        description: "Shrimp, chicken and beef sautéed with broccoli, baby corn and mushrooms in a rich brown sauce.",
+        spice: null
+    },
+    {
+        name: "Happy Family",
+        price: 23,
+        description: "Fillet mignon, shrimp and chicken sautéed with assorted vegetables in a white wine sauce.",
+        spice: null
+    },
+    {
+        name: "House Special Steak Kew",
+        price: 23,
+        description: "Fillet mignon sautéed with vegetables in gourmet brown sauce.",
+        spice: null
+    }, {
+        name: "Perfect Match",
+        price: 18,
+        description: "Chicken and shrimp sautéed with peanut flavored brown sauce.",
+        spice: null
+    },
+    {
+        name: "General Tso's Chicken",
+        price: 17,
+        description: "Fillet mignon and shrimp sautéed w. broccoli, pepper & mushroom in brown sauce.",
+        spice: null
+    },
+    {
+        name: "Shrimp and Steak",
+        price: 23,
+        description: "Chicken and shrimp sautéed with snow peas & zucchini in Szechuan chili sauce.",
+        spice: null
+    },
+    {
+        name: "Chicken with Pine Nuts",
+        price: 17,
+        description: "Sautéed chicken and mixed veg. in brown sauce, topped with pine nuts.",
+        spice: null
+    },
+    {
+        name: "Hunan Fish Fillet",
+        price: 18,
+        description: "Lightly fried fillet of flounder, showered with rich Hunan chili sauce.",
+        spice: null
+    },
+    {
+        name: "Prawn and Phoenix",
+        price: 18,
+        description: "Chicken & shrimp sautéed w. snow peas & zucchini in Szechuan chili sauce.",
+        spice: null
+    },
+    {
+        name: "Chicken Country Style",
+        price: 17,
+        description: "Chicken strips sautéed w. onion, pepper & baby corn in Chinese satay sauce.",
+        spice: null
+    },
+    {
+        name: "Empress Chicken",
+        price: 17,
+        description: "Chicken w. broccoli, water chestnuts and mushroom in Hunan chili sauce.",
+        spice: null
+    },
+    {
+        name: "Tofu casserole with Seafood",
+        price: 19,
+        description: "Fried tofu, shrimp, scallops, squid, black mushrooms, water chestnuts, cabbage and baby bok choy in a light brown sauce"
+    }
+]
