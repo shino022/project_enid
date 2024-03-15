ae6f41b6709ac3efcad8ab5afdfe3f37cf60a335
eynid <eeihou312@gmail.com>
2024-03-15 17:06:44 -0400

    added soups

--- client/src/jsFiles/appetizers.js
+++ client/src/jsFiles/appetizers.js
@@ -1,58 +1,82 @@
 export const appetizers = [
     {
-        name: "Egg Roll",
-        price: 3
+        name: "Egg Roll (1)",
+        price: 3,
+        spice: false
     },
     {
-        name: "Vegetable Roll",
-        price: 2.5
+        name: "Vegetable Roll (1)",
+        price: 2.5,
+        spice: false
     },
     {
-        name: "Barbecued Spare Ribs (5 pc)",
-        price: 12
+        name: "Barbecued Spare Ribs (5 pcs)",
+        price: 12,
+        spice: false
     },
     {
         name: "Boneless Ribs",
-        price: 10
+        price: 11,
+        spice: false
     },
     {
-        name: "Fried or Boiled Dumplings (Pork or Veg)",
-        price: 9
+        name: "Fried Shrimp Balls (6 pcs)",
+        price: 9,
+        spice: false
+    },
+    {
+        name: "Fried or Boiled Pork/Vegatable Dumplings (6 pcs)",
+        price: 9,
+        spice: false
     },
     {
         name: "Cold Noodles with Sesame Sauce",
-        price: 9
+        price: 9,
+        spice: false
     },
     {
-        name: "Wontons with Hot Sesame Sauce (8 pc)",
-        price: 9
+        name: "Wontons with Hot Sesame Sauce (6 pcs)",
+        price: 9,
+        spice:true
     },
     {
-        name: "Chicken Skewers (4 pc)",
-        price: 10
+        name: "Chicken Skewers (4 pcs)",
+        price: 10,
+        spice: false
     },
     {
-        name: "Beef Skewers (4 pc)",
-        price: 11
+        name: "Beef Skewers (4 pcs)",
+        price: 11,
+        spice: false
     },
     {
         name: "Scallion Pancakes",
-        price: 7
+        price: 7,
+        spice: false
     },
     {
-        name: "Shrimp Toast (4)",
-        price: 9
+        name: "Shrimp Toast (4 pcs)",
+        price: 9,
+        spice: false
     },
     {
         name: "Edamame",
-        price: 7
+        price: 7,
+        spice: false
     },
     {
-        name: "Assorted Hot Appetizers (2)",
-        price: 20
+        name: "Sliced Roast Pork",
+        price: 11,
+        spice: false
     },
     {
-        name: "Crab Rangoon",
-        price: 10
+        name: "Assorted Hot Appetizers (2 pcs)",
+        price: 20,
+        spice: false
+    },
+    {
+        name: "Crab Rangoon ",
+        price: 10,
+        spice: false
     },
 ]
---
+++ client/src/jsFiles/soup.js
@@ -0,0 +1,47 @@
+export const soups = [
+    {
+        name: "Egg Drop Soup (sm)",
+        price: 3.5,
+        spice: false
+    },
+    {
+        name: "Wonton Soup (sm)",
+        price: 3.5,
+        spice: false
+    },
+    {
+        name: "Hot and Sour Soup (sm)",
+        price: 3.5,
+        spice: true
+    },
+    {
+        name: "Noodle or Rice Soup (sm)",
+        price: 3.5,
+        spice: false
+    },
+    {
+        name: "House Special Soup (lg)",
+        price: 8.5,
+        spice: false
+    },
+    {
+        name: "Velvet Chicken and Corn Soup (lg)",
+        price: 8,
+        spice: false
+    },
+    {
+        name: "Bean Curd and Cabbage Soup (lg)",
+        price: 8,
+        spice: false
+    },
+    {
+        name: "Chicken Noodle (lg)",
+        price: 8,
+        spice: false
+    },
+    {
+        name: "Chicken Rice Soup (lg)",
+        price: 8,
+        spice: false
+    },
+]
