52876d5a565a98495c55c2375d58b2199bb17658
AmeerSyed7340 <Ameer.Syed40@qmail.cuny.edu>
2024-03-16 08:53:46 -0400

    Updated footer alignment and font family to Cormorant upright

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,16 @@
 import '../styles/Footer.css'
 function Footer() {
-    return (
-      <footer className="footer">
-        <div className="footer_content" id="footer_target">
-          <p className="shopping-center"> SCARSDALE GOLDEN HORSESHOE SHOPPING CENTER</p>  {/* Update restaurant name if different */}
-          <address>
-            1122 Wilmot Road<br />
-            Scarsdale, NY 10583
-          </address>
-          <a href="tel:914-472-4774">(914) 472-4774</a> {/* Update phone number */}
-          <a href="tel:914-472-4774">(914) 472-4775</a> {/* Update phone number */}
-          
-        </div>
-      </footer>
-    );
-  }
-  
-  export default Footer;
-  
+  return (
+    <footer className="footer" id="footer_target">
+      <p className="shopping-center"> SCARSDALE GOLDEN HORSESHOE SHOPPING CENTER</p>  {/* Update restaurant name if different */}
+      <address className='address'>
+        1122 Wilmot Road<br />
+        Scarsdale, NY 10583
+      </address>
+      <a href="tel:914-472-4774">(914) 472-4774</a> {/* Update phone number */}
+      <a href="tel:914-472-4774">(914) 472-4775</a> {/* Update phone number */}
+    </footer>
+  );
+}
+
+export default Footer;
