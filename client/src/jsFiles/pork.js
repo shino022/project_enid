0af41bb1d69fd27bf5eef911f8a8aef734de716b
eynid <eeihou312@gmail.com>
2024-03-17 18:16:29 -0400

    prepush commit

--- client/src/jsFiles/pork.js
+++ client/src/jsFiles/pork.js
@@ -2,42 +2,42 @@ export const pork = [
     {
         name: "Sliced Roast Pork w. Mixed Vegetable",
         dine_In_price: 16.50,
-        take_out_price: 1,
+        take_out_price: 16,
         spice: null,
         gluten: false
       },
       {
         name: "Moo Shu Pork (with 2 Pancakes)",
         dine_In_price: 16.50,
-        take_out_price: 1,
+        take_out_price: 16, 
         spice: null,
         gluten: false
       },
       {
         name: "Twice Cooked Pork",
         dine_In_price: 16.50,
-        take_out_price: 1,
+        take_out_price: 16,
         spice: true,
         gluten: false
       },
       {
         name: "Shredded Pork with Garlic Sauce",
         dine_In_price: 16.50,
-        take_out_price: 1,
+        take_out_price: 16,
         spice: true,
         gluten: false
       },
       {
         name: "Shredded Pork with Peking Sauce",
         dine_In_price: 16.50,
-        take_out_price: 1,
+        take_out_price: 16,
         spice: null,
         gluten: false
       },
       {
         name: "Sweet and Sour Pork",
         dine_In_price: 16.50,
-        take_out_price: 1,
+        take_out_price: 16,
         spice: null,
         gluten: false
       }
